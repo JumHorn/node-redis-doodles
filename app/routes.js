591486be73f09897b2c48cd6021c6145d33cfd9d
JumHorn <JumHorn@gmail.com>
2023-04-26 09:51:14 +0000

    base64 transfer

--- app/routes.js
+++ app/routes.js
@@ -7,13 +7,13 @@ module.exports = function (app) {
     app.get('/doodles', function(req, res) {
         // 从Redis中获取网页
         redisClient.get('doodles.html', function(err, reply) {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else if (reply === null) {
-            res.sendFile(path.join(__dirname, "../public/doodles.html"));
-        } else {
-            res.send(reply);
-        }
+            if (err) {
+                res.status(500).send('Internal Server Error');
+            } else if (reply === null) {
+                res.sendFile(path.join(__dirname, "../public/doodles.html"));
+            } else {
+                res.send(replyBuffer.from(reply, 'base64').toString('utf8'));
+            }
         });
     });
     app.post('/doodles', function(req, res) {
@@ -21,12 +21,12 @@ module.exports = function (app) {
     
         // 将数据存储到Redis中
         redisClient.set('doodles.html', content, function(err, result) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.send('OK');
-        }
+            if (err) {
+                console.error(err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.send('OK');
+            }
         });
     });
 };
