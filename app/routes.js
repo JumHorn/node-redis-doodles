74f67703bb84550d8a3c7fe00a783c58cbe30d6b
JumHorn <JumHorn@gmail.com>
2023-04-28 03:09:44 +0000

    identity different user with username

--- app/routes.js
+++ app/routes.js
@@ -3,30 +3,65 @@ const path = require('path');
 const { redisClient } = require("../config/redis");
 
 module.exports = function (app) {
-    // 处理POST请求
+    // 处理GET请求
     app.get('/doodles', function(req, res) {
-        // 从Redis中获取网页
-        redisClient.get('doodles.html', function(err, reply) {
-            if (err) {
-                res.status(500).send('Internal Server Error');
-            } else if (reply === null) {
-                res.sendFile(path.join(__dirname, "../public/doodles.html"));
-            } else {
-                res.send(reply);
-            }
-        });
+        // 先取浏览器请求，后取cookies
+        const username = req.query.username;
+        if (typeof username === 'undefined') {
+            username = req.cookie.user;
+        }
+
+        if (typeof username === 'undefined') {
+            // 从Redis中获取网页
+            redisClient.get('doodles.html', function(err, reply) {
+                if (err) {
+                    res.status(500).send('Internal Server Error');
+                } else if (reply === null) {
+                    res.sendFile(path.join(__dirname, "../public/doodles.html"));
+                } else {
+                    res.send(reply);
+                }
+            });
+        } else {
+            // 从Redis中获取网页
+            redisClient.hget('doodles', username, function(err, reply) {
+                res.cookie('username', username, { maxAge: 315360000000 });//设置cookies
+                if (err) {
+                    res.status(500).send('Internal Server Error');
+                } else if (reply === null) {
+                    res.sendFile(path.join(__dirname, "../public/doodles.html"));
+                } else {
+                    res.send(reply);
+                }
+            });
+        }
     });
+    
+    // 处理POST请求
     app.post('/doodles', function(req, res) {
         const content = req.body;
-    
-        // 将数据存储到Redis中
-        redisClient.set('doodles.html', content, function(err, reply) {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Internal Server Error');
-            } else {
-                res.send('OK');
-            }
-        });
+        const username = req.cookie.user;
+        
+        if (typeof username === 'undefined') {
+            // 将数据存储到Redis中
+            redisClient.set('doodles.html', content, function(err, reply) {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.send('OK');
+                }
+            });
+        } else {
+            // 将数据存储到Redis中
+            redisClient.hset('doodles', username, content, function(err, reply) {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.send('OK');
+                }
+            });
+        }
     });
 };
