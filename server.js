67365c69fdb9f6be284489cbff7c7ccf5a191e32
JumHorn <JumHorn@gmail.com>
2024-03-13 22:45:10 -0400

    log req to console

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 
 const { PORT } = require("./config/config");
 
 // 创建Express实例
 const app = express();
-
+// 打印请求日志
+app.use(morgan('dev'));
 // 解析请求体中的TEXT数据
 app.use(bodyParser.text({ limit: '20mb' }));
 // app.use(bodyParser.json({ limit: '20mb' }));
 
 // 解析请求中的cookies
 app.use(cookieParser());
-
 // routes
 require('./app/routes.js')(app);
 
